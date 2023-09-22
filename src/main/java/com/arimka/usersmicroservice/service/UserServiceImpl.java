86b94301211607b492e951fe2ff2aed01e2c24ae
stonebridge6 <aristidembaya6@gmail.com>
2023-09-22 14:03:31 +0100

    warriors

---
+++ src/main/java/com/arimka/usersmicroservice/service/UserServiceImpl.java
@@ -0,0 +1,50 @@
+package com.arimka.usersmicroservice.service;
+
+import com.arimka.usersmicroservice.entities.Role;
+import com.arimka.usersmicroservice.entities.User;
+import com.arimka.usersmicroservice.repository.RoleRepository;
+import com.arimka.usersmicroservice.repository.UserRepository;
+import jakarta.transaction.Transactional;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Transactional
+@Service
+public class UserServiceImpl implements UserService{
+    @Autowired
+    UserRepository userRep;
+    @Autowired
+    RoleRepository roleRep;
+    @Autowired
+    BCryptPasswordEncoder bCryptPasswordEncoder;
+    @Override
+    public User saveUser(User user) {
+        user.setPassword(bCryptPasswordEncoder.encode(user.getPassword()));
+        return userRep.save(user);
+    }
+    @Override
+    public User addRoleToUser(String username, String rolename) {
+        User usr = userRep.findByUsername(username);
+        Role r = roleRep.findByRole(rolename);
+        usr.getRoles().add(r);
+        return usr;
+    }
+
+    @Override
+    public List<User> findAllUsers() {
+        return userRep.findAll();
+    }
+
+    @Override
+    public Role addRole(Role role) {
+        return roleRep.save(role);
+    }
+    @Override
+    public User findUserByUsername(String username) {
+        return userRep.findByUsername(username);
+    }
+}
+
